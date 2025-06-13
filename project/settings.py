7e70b514118b605da089491eea23bb2f9549ccab
siddu <siddusidharth6300@gmail.com>
2025-06-13 20:19:49 +0530

    fix: added static_root in settings

--- project/settings.py
+++ project/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
@@ -120,6 +121,9 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # STATICFILES_DIRS = [BASE_DIR / 'static'] 
 # STATIC_ROOT = BASE_DIR / 'staticfiles' 

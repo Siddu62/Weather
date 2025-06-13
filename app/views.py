69969278e280abc5f581205a679d3c242f20d830
siddu <siddusidharth6300@gmail.com>
2025-06-13 17:40:00 +0530

    project successfuly complete

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+def index(request):
+    city = request.GET.get('city')
+    api_key="8f028d5c51044649d8dc34b48eca1ea1"
+    api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    print(api_url)
+    api = requests.get(api_url).json()
+    temperature=api['main']['temp']
+    city = api['name']
+    country = api['sys']['country']
+    return render(request,'index.html',{'temperature':temperature, 'city':city, 'country':country})
+

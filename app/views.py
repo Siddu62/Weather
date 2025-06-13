2991385d41fb114bf3add915d02aa7371af3e69b
siddu <siddusidharth6300@gmail.com>
2025-06-13 19:51:20 +0530

    Added gunicorn to requirements

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 import requests
+import os
 # Create your views here.
 def index(request):
     city = request.GET.get('city')
-    api_key="8f028d5c51044649d8dc34b48eca1ea1"
+    # api_key="8f028d5c51044649d8dc34b48eca1ea1"
+    api_key = os.getenv("OPENWEATHER_API_KEY")
+    
     api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     print(api_url)
     api = requests.get(api_url).json()
